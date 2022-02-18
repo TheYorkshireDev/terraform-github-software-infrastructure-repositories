fc6ae44b169f9b71b05efcd3796fad216a0ed016
Steven Cooney <30004860+TheYorkshireDev@users.noreply.github.com>
2022-02-18 18:22:55 +0000

    Add Inital GitHub Repo

---
+++ software-repository.tf
@@ -0,0 +1,8 @@
+resource "github_repository" "software_repo" {
+  name = var.name
+}
+
+resource "github_branch_default" "software_repo_default_branch" {
+  repository = github_repository.software_repo.name
+  branch     = var.default_branch
+}
